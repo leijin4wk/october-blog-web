8956a1afcbf5ac5518a9ef7200be8d0cb5496ffb
leijin4wk <leijin4wk@163.com>
2019-12-29 09:01:03 +0800

    添加歌词动态效果

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
     app.use(proxy("/api", {
-        target: "https://www.octlr.com",
+        // target: "https://www.octlr.com",
+        target: "https://localhost:8080",
         changeOrigin: true,
         pathRewrite: {
             "^/api": "/api"
